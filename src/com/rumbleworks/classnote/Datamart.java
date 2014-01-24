a19a26f7fa26e9764f28e74045cc0d3ba5ad2afd
ogoldbart3 <oliver.goldbart@gmail.com>
2014-01-24 12:35:14 -0500

    Fixed push
    So I'm not 100% sure how to import a java project into eclipse from just
    the files, but this github is now literally just the files that work in
    my eclipse workspace.  Will help fix when people need it

---
+++ src/com/rumbleworks/classnote/Datamart.java
@@ -0,0 +1,14 @@
+package com.rumbleworks.classnote;
+
+import java.util.ArrayList;
+
+public class Datamart {
+
+	ArrayList<Course> courseList;
+	
+	public Datamart() {
+		courseList = new ArrayList<Course>();
+	}
+	
+	
+}
