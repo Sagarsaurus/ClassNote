9daedc5a33e9d412166d1cb5c53090cf4195dac9
ogoldbart3 <oliver.goldbart@gmail.com>
2014-01-24 12:13:58 -0500

    Hypothetical push
    This might bust everything, but hopefully this is the most recent code

---
+++ src/com/rumbleworks/classnotepractice/Assignment.java
@@ -0,0 +1,16 @@
+package com.rumbleworks.classnotepractice;
+
+import java.util.Date;
+
+public class Assignment extends GradedWork {
+
+	/** @param  String name, String description, Boolean isRead, Date dueDate, int courseNumber, double pointsEarned, double pointsPossible, double overallScore, double percentageOfFinal
+	 * An assignment is a normal, t-square assignment that will be shown in the assignments list
+	 * and be able to be added to the final grade of the course
+	 *  @return void */ 
+	
+	public Assignment( String name, String description, Boolean isRead, Date dueDate, int courseNumber, double pointsEarned, double pointsPossible, double overallScore, double percentageOfFinal ) {
+		super( name, description, isRead, dueDate, courseNumber, pointsEarned, pointsPossible, overallScore, percentageOfFinal );
+	}
+	
+}
---
+++ src/com/rumbleworks/classnotepractice/Datamart.java
@@ -0,0 +1,14 @@
+package com.rumbleworks.classnotepractice;
+
+import java.util.ArrayList;
+
+public class Datamart {
+
+	ArrayList<Course> courseList;
+	
+	public Datamart() {
+		courseList = new ArrayList<Course>();
+	}
+	
+	
+}
