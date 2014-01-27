048fb10d562e291b1bb3f11f2c339c9e2b06332e
Josh Williams <joshuaw92@gmail.com>
2014-01-27 10:39:40 -0500

    added getters and setters to Course and Datamart. Made Datamart a singleton

--- src/com/rumbleworks/classnote/Course.java
+++ src/com/rumbleworks/classnote/Course.java
@@ -1,15 +1,15 @@
 package com.rumbleworks.classnote;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Course {
-
-	String courseName;
-	String courseSchool;
-	int courseNumber;
+    private String courseName;
+	private String courseSchool;
+	private int courseNumber;
 	
-	ArrayList<GradedWork> gradedWorkList;
-	ArrayList<Announcement> announcementList;
+	private ArrayList<Assignment> assignmentList;
+	private ArrayList<Announcement> announcementList;
 	
 	/** @param String courseName, String courseSchool, int courseNumber
 	 * A course is a course at a university or college.  Here, it exists as a set of information 
@@ -18,14 +18,42 @@ public class Course {
 	 * and the other for Announcements from the professor
 	 *  @return void */ 
 	
-	public Course( String courseName, String courseSchool, int courseNumber ) {
+	public Course(String courseName, String courseSchool, int courseNumber) {
 		this.courseName = courseName;
 		this.courseSchool = courseSchool;
 		this.courseNumber = courseNumber;
 		
-		this.gradedWorkList = new ArrayList<GradedWork>();
+		this.assignmentList = new ArrayList<Assignment>();
 		this.announcementList = new ArrayList<Announcement>();
-		
 	}
-	
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseSchool() {
+        return courseSchool;
+    }
+
+    public int getCourseNumber() {
+        return courseNumber;
+    }
+
+    public List<Assignment> getAssignmentList() {
+        return assignmentList;
+    }
+
+    public List<Announcement> getAnnouncementList() {
+        return announcementList;
+    }
+
+    public void addAssignment(Assignment assignment) {
+        assignmentList.add(assignment);
+    }
+
+    public void addAnnouncement(Announcement announcement) {
+        announcementList.add(announcement);
+    }
+
+
 }
--- src/com/rumbleworks/classnote/Datamart.java
+++ src/com/rumbleworks/classnote/Datamart.java
@@ -1,14 +1,30 @@
 package com.rumbleworks.classnote;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Datamart {
 
-	ArrayList<Course> courseList;
+    static Datamart instance;
+
+	private ArrayList<Course> courseList;
 	
 	public Datamart() {
 		courseList = new ArrayList<Course>();
 	}
-	
-	
+
+    public static Datamart getInstance() {
+        if (instance == null) {
+            instance = new Datamart();
+        }
+        return instance;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void addCourse(Course c) {
+        courseList.add(c);
+    }
 }
