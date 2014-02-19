45d0affa6a30fd981209a89783d89e69bf2a5741
ogoldbart3 <oliver.goldbart@gmail.com>
2014-02-18 22:08:05 -0500

    Testing out being able to commit directly through android studio

--- src/com/rumbleworks/classnote/Test.java
+++ src/com/rumbleworks/classnote/Test.java
@@ -7,5 +7,6 @@ public class Test extends GradedWork {
 	public Test(String name, String description, Boolean isRead, Date dueDate, String courseId, double pointsEarned, double pointsPossible, double overallScore, double percentageOfFinal ) {
 		super(name, description, isRead, dueDate, courseId, pointsEarned, pointsPossible, overallScore, percentageOfFinal );
 	}
-	
+
+
 }
