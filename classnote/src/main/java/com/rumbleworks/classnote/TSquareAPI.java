68669a915a429c8867bd18dbd414c75ae8c91764
Samarth <samarth.agarwal1993@gmail.com>
2014-03-26 01:20:05 -0400

    Empty overlay added.

--- classnote/src/main/java/com/rumbleworks/classnote/TSquareAPI.java
+++ classnote/src/main/java/com/rumbleworks/classnote/TSquareAPI.java
@@ -25,11 +25,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.CookieStore;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.util.concurrent.Future;
 
 
 public class TSquareAPI {
