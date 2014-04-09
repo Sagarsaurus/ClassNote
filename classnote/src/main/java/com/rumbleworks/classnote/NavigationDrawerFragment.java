9c7f8317fd4ca22716f339dbada1be2e0594c389
Sagarsaurus <Sagarsaurus@github.com>
2014-04-09 14:11:35 -0400

    Renamed activities and text on buttons in application to mitigate confusion.

--- classnote/src/main/java/com/rumbleworks/classnote/NavigationDrawerFragment.java
+++ classnote/src/main/java/com/rumbleworks/classnote/NavigationDrawerFragment.java
@@ -252,7 +252,7 @@ public class NavigationDrawerFragment extends Fragment {
         }
 
         if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Everything is up-to-date.", Toast.LENGTH_SHORT).show();
             return true;
         }
 
