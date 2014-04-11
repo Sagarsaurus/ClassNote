29939b9dbc3e786fbceef19d1c8651fb011bf041
Josh Williams <joshuaw92@gmail.com>
2014-04-11 08:56:37 -0400

    remove unused menus from detail activities

--- classnote/src/main/java/com/rumbleworks/classnote/AnnouncementDetailActivity.java
+++ classnote/src/main/java/com/rumbleworks/classnote/AnnouncementDetailActivity.java
@@ -23,25 +23,4 @@ public class AnnouncementDetailActivity extends ActionBarActivity {
         description.setText("Message: \n\n"+b.getString("DESCRIPTION"));
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.announcement_details, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
