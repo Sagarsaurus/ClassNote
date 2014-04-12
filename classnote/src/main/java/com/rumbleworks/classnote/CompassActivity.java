70535cb31771f71bcef48a7d704c606352bac9f5
Josh Williams <joshuaw92@gmail.com>
2014-04-12 11:59:59 -0400

    add back commented out line

--- classnote/src/main/java/com/rumbleworks/classnote/CompassActivity.java
+++ classnote/src/main/java/com/rumbleworks/classnote/CompassActivity.java
@@ -136,8 +136,7 @@ public class CompassActivity extends ActionBarActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
-//            TSquareAPI.refreshAll();
-            TSquareAPI.showLoginToRefresh();
+            TSquareAPI.refreshAll();
             return true;
         }
         if (id == R.id.action_help) {
