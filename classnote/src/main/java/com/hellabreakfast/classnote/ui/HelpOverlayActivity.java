3eba00c1ca2377a81486802cc287adcf520e714a
Josh Williams <joshuaw92@gmail.com>
2014-04-25 10:35:10 -0400

    made the help overlay activity use string resources

--- classnote/src/main/java/com/hellabreakfast/classnote/ui/HelpOverlayActivity.java
+++ classnote/src/main/java/com/hellabreakfast/classnote/ui/HelpOverlayActivity.java
@@ -8,20 +8,10 @@ import android.widget.TextView;
 import com.hellabreakfast.classnote.model.Datamart;
 import com.hellabreakfast.classnote.R;
 
-
+/**
+ * This activity shows the help overlay for the current screen in a transparent view over the screen.
+ */
 public class HelpOverlayActivity extends ActionBarActivity {
-
-    private String[][] helpArray = {    { "Tap the top left of the screen or swipe from the left to open the menu for the application", "All announcements are present on this page and are sorted from most to least recent", "", "", "Tap anywhere to dismiss instructions" },
-                                        { "Open the menu to add new assignments", "Tap on an assignment to view in more detail", "", "", "Tap anywhere to dismiss instructions"},
-                                        { "Open the menu to add new assignments", "Tap on an assignment to view in more detail", "", "", "Tap anywhere to dismiss instructions"},
-                                        { "Tap a day to create a new assignment due on that date", "Slide up or down to go from one month to another", "", "", "Tap anywhere to dismiss instructions" },
-                                        { "Graded assignments appear here after being marked as returned on T-Square", "Manually added assignments appear here after their due date", "You can tap assignments you have added to grade them manually", "", "Tap anywhere to dismiss instructions"},
-                                        { "Enter basic information about class including course number, name of assignment, and a brief description", "Either select the month and day on the left or select a date from the calendar on the right to select the due date", "Scroll down and set what time the assignment is due", "Tap “Save Assignment” when you are finished", "Tap anywhere to dismiss instructions"},
-                                        { "", "", "", "", "" },
-                                        { "", "", "", "", "" } };
-
-    public int currentScreen;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +21,7 @@ public class HelpOverlayActivity extends ActionBarActivity {
         String title = getIntent().getStringExtra("title");
         if (title != null) setTitle(title);
 
-        currentScreen = Datamart.getInstance().getCurrentScreen();
+        int currentScreen = Datamart.getInstance().getCurrentScreen();
 
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         TextView textView2 = (TextView) findViewById(R.id.textView2);
@@ -39,13 +29,22 @@ public class HelpOverlayActivity extends ActionBarActivity {
         TextView textView4 = (TextView) findViewById(R.id.textView4);
         TextView textView5 = (TextView) findViewById(R.id.textView5);
 
-        textView1.setText( helpArray[ currentScreen ][0] );
-        textView2.setText( helpArray[ currentScreen ][1] );
-        textView3.setText( helpArray[ currentScreen ][2] );
-        textView4.setText( helpArray[ currentScreen ][3] );
-        textView5.setText( helpArray[ currentScreen ][4] );
+        String[][] helpArray = {
+                getResources().getStringArray(R.array.help_items_announcements),
+                getResources().getStringArray(R.array.help_items_assignments),
+                getResources().getStringArray(R.array.help_items_assignments),
+                getResources().getStringArray(R.array.help_items_add_assignment)
+        };
+        textView1.setText( helpArray[currentScreen][0] );
+        textView2.setText( helpArray[currentScreen][1] );
+        textView3.setText( helpArray[currentScreen][2] );
+        textView4.setText( helpArray[currentScreen][3] );
+        textView5.setText( helpArray[currentScreen][4] );
     }
 
+    /**
+     * The user tapped to dismiss, so finish the activity.
+     */
     public void understood(View view){
         finish();
     }
